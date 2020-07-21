c3102cb506fd1c30b5ce644f380e5a809728e058
Zhenduo Wang <30311623+zhenduow@users.noreply.github.com>
2020-07-21 16:46:15 -0600

    Update gen_DPR_data.py

--- gen_DPR_data.py
+++ gen_DPR_data.py
@@ -19,54 +19,62 @@ def processing(input_text, max_len):
     result = result[:max_len]
     return result.strip()
 
-with open('MSDialog-Complete.json') as f:
+with open('MSDialog-Intent.json') as f:
     data = json.load(f)
 
     for k in data.keys():
         final_answer = ''
-        is_answer_label = [1 if ut['is_answer'] == 1 else 0 for ut in data[k]['utterances']]
-        has_answer = np.sum(is_answer_label)
-        if has_answer == 0:
-            continue
-        conversations = []
-        for utterance_id, utterance in enumerate(data[k]['utterances']):
-            unprocessed = utterance['utterance']
-            processed = processing(unprocessed, max_len).strip()
-            if conversations== []:
-                conversations = [data[k]['title'] + '. [SEP] ' + processed]
-                #conversations = [processed]
-            elif utterance['actor_type'] == role:
-                # concatenate all consecutive utterances from one role together
-                conversations[-1] += '. '
-                conversations[-1] += processed
-            else:
-                if utterance['actor_type'] == 'User' and utterance_id == len(data[k]['utterances']) -1 :
-                    break
-                conversations.append(processed)
+        #is_answer_label = [1 if ut['is_answer'] == 1 else 0 for ut in data[k]['utterances']]
+        #has_answer = np.sum(is_answer_label)
+        #if has_answer == 0:
+        #    continue
+        intents = []
+        for utterance in data[k]['utterances']:
+            intents.extend(utterance['tags'].split())
+        if set(['CQ', 'IR', 'FD']) & set(intents):
+            conversations = []
+            role = 'User'
+            for utterance_id, utterance in enumerate(data[k]['utterances']):
+                if set(utterance['tags'].split()).issubset(set(['GG', 'JK', 'PF'])): 
+                    # remove greetings or junk messages
+                    continue
+                unprocessed = utterance['utterance']
+                processed = processing(unprocessed, max_len).strip()
+                if conversations== []:
+                    conversations = [data[k]['title'] + '. [SEP] ' + processed]
+                    #conversations = [processed]
+                elif utterance['actor_type'] == role:
+                    # concatenate all consecutive utterances from one role together
+                    conversations[-1] += '. '
+                    conversations[-1] += processed
+                else:
+                    if utterance['actor_type'] == 'User' and utterance_id == len(data[k]['utterances']) -1 :
+                        break
+                    conversations.append(processed)
                     
             # adding any agent response to response pool and update it as the final_answer
-            if utterance['is_answer'] == 1:
-                final_answer = processed
-                break
+                if utterance['is_answer'] == 1:
+                    final_answer = processed
+                    break
 
-            role = utterance['actor_type']
+                role = utterance['actor_type']
         
-        if len(conversations) <= 2:
-            continue
+            if len(conversations) %2:
+                continue
 
-        for final_ut_id in range(len(conversations)):
-            if final_ut_id % 2:
-                if final_ut_id != len(conversations) -1:
-                    response_data.append([k, conversations[final_ut_id], '']) # otherwise append data[k]['title]
-            else:
-                if final_ut_id == 0:
-                    question_data.append(conversations[final_ut_id])
+            for final_ut_id in range(len(conversations)):
+                if final_ut_id % 2:
+                    if final_ut_id != len(conversations) -1:
+                        response_data.append([k, conversations[final_ut_id], '']) # otherwise append data[k]['title]
                 else:
-                    question_data.append(' [SEP] '.join(conversations[:final_ut_id + 1]))
-                question_id.append(k)
+                    if final_ut_id == 0:
+                        question_data.append(conversations[final_ut_id])
+                    else:
+                        question_data.append(' [SEP] '.join(conversations[:final_ut_id + 1]))
+                    question_id.append(k)
                     
-        if final_answer != '':
-            answer_data.append([k, final_answer, '']) 
+            if final_answer != '':
+                answer_data.append([k, final_answer, '']) 
 
 # check response pool and answer pool are disjoint. It does happen because Microsoft agents use template answers very occasionally.
 #print(set.intersection(set([i[1] for i in response_data]),set([i[1] for i in answer_data])))

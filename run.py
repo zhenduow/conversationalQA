b736fa8225214d7b77d69c649040008b9a417f78
Zhenduo Wang <30311623+zhenduow@users.noreply.github.com>
2020-08-25 17:16:17 -0600

    Update run.py

--- run.py
+++ run.py
@@ -19,7 +19,7 @@ cq_reward = 0.49
 cq_penalty = -0.5
 agent_gamma = 0.25
 pretrain_iter = 3
-train_iter = 10
+train_iter = 20
 dataset_size = 1496
 train_test_split_ratio = 0.8
 train_size = int(train_test_split_ratio * dataset_size)
@@ -35,7 +35,7 @@ if __name__ == '__main__':
     train_ids = conversation_ids[:train_size]
     test_ids = conversation_ids[train_size:]
     user = User(dataset, cq_reward = cq_reward, cq_penalty = cq_penalty)
-    agent = Agent(lr=0.0001, input_dims = 2* use_top_k * observation_dim, n_actions=action_num, gamma = agent_gamma)
+    agent = Agent(lr=1e-4, input_dims = 2* use_top_k * observation_dim, n_actions=action_num, gamma = agent_gamma)
     memory = {}
 
     parser = argparse.ArgumentParser()
@@ -73,16 +73,16 @@ if __name__ == '__main__':
 
 
     for i in range(train_iter):
-        train_scores, train_q0_scores, train_q1_scores, train_q2_scores = [],[],[],[]
-        train_worse, train_q0_worse, train_q1_worse, train_q2_worse = [],[],[],[]
-        train_correct, train_q0_correct, train_q1_correct, train_q2_correct = [],[],[],[]
+        train_scores, train_q0_scores, train_q1_scores, train_q2_scores, train_oracle_scores = [],[],[],[],[]
+        train_worse, train_q0_worse, train_q1_worse, train_q2_worse, train_oracle_worse = [],[],[],[],[]
+        train_correct, train_q0_correct, train_q1_correct, train_q2_correct, train_oracle_correct = [],[],[],[],[]
         for conv_serial, conv_id in enumerate(train_ids):
             obs = user.initialize_state(conv_id)
             ignore_questions = []
             n_round = 0
             q_done = False
             stop = False
-            print('-------- train conversation %.0f/%.0f --------' % (conv_serial, len(train_ids)))
+            print('-------- train conversation %.0f/%.0f --------' % (conv_serial + 1, len(train_ids)))
             while not q_done:
                 print('-------- round %.0f --------' % (n_round))
                 if obs in memory.keys():
@@ -121,9 +121,10 @@ if __name__ == '__main__':
                         model_file = 'zoo:pretrained_transformers/model_poly/msdialogquestion',  \
                         encode_candidate_vecs = 'true', eval_candidates ='fixed',  \
                         fixed_candidates_path = '/raid/zhenduow/conversationalQA/data/' + exp_name + '/retrieved_question', \
+                            force_fp16_tokens = True,\
                         human_input=obs, fixed_candidate_vecs = 'replace')
                     answer = Interactive.main(model = 'transformer/polyencoder', \
-                        model_file = 'zoo:pretrained_transformers/model_poly/msdialogmodel',  \
+                        model_file = 'zoo:pretrained_transformers/model_poly/msdialoganswer',  \
                         encode_candidate_vecs = 'true', eval_candidates ='fixed',  \
                         fixed_candidates_path = '/raid/zhenduow/conversationalQA/data/' + exp_name + '/retrieved_answer', \
                         human_input=obs,  fixed_candidate_vecs = 'replace')
@@ -173,9 +174,10 @@ if __name__ == '__main__':
                             model_file = 'zoo:pretrained_transformers/model_poly/msdialogquestion',  \
                             encode_candidate_vecs = 'true', eval_candidates ='fixed',  \
                             fixed_candidates_path = '/raid/zhenduow/conversationalQA/data/' + exp_name + '/retrieved_question', \
+                            force_fp16_tokens = True,\
                             human_input=obs_, fixed_candidate_vecs = 'replace')
                         answer_ = Interactive.main(model = 'transformer/polyencoder', \
-                            model_file = 'zoo:pretrained_transformers/model_poly/msdialogmodel',  \
+                            model_file = 'zoo:pretrained_transformers/model_poly/msdialoganswer',  \
                             encode_candidate_vecs = 'true', eval_candidates ='fixed',  \
                             fixed_candidates_path = '/raid/zhenduow/conversationalQA/data/' + exp_name + '/retrieved_answer', \
                             human_input=obs_,  fixed_candidate_vecs = 'replace')
@@ -194,7 +196,7 @@ if __name__ == '__main__':
                     if action == 0 and answer_reward == 1.0:
                         train_correct.append(conv_id) 
                     train_worse.append(1 if (action == 0 and answer_reward < float(1/use_top_k) and question_reward == cq_reward) \
-                        or (action == 1 and question_reward == cq_penalty) else 0)
+                        or (action == 1 and answer_reward > 0 and question_reward == cq_penalty) else 0)
                 if n_round == 0:
                     train_q0_scores.append(answer_reward)
                     train_q0_worse.append(1 if answer_reward < float(1/use_top_k) and question_reward == cq_reward else 0)
@@ -226,32 +228,41 @@ if __name__ == '__main__':
         assert len(train_scores) == len(train_q0_scores)
         assert len(train_q1_scores) == len(train_q0_scores)
         assert len(train_q1_scores) == len(train_q2_scores)
+        for oi in range(len(train_scores)):
+            train_oracle_scores.append(max(train_q0_scores[oi], train_q1_scores[oi], train_q2_scores[oi]))
+            train_oracle_worse.append(min(train_q0_scores[oi], train_q1_scores[oi], train_q2_scores[oi]))
+        train_oracle_correct = list(set(train_correct + train_q0_correct + train_q2_correct))
 
-        print("Train epoch %.0f, acc %.0f, avgmrr %.6f, worse decisions %.0f" % 
-            (i, np.sum([1 if score == 1 else 0 for score in train_scores]), np.mean(train_scores), np.sum(train_worse)))
+        print("Train epoch %.0f, acc %.6f, avgmrr %.6f, worse decisions %.6f" % 
+            (i, np.mean([1 if score == 1 else 0 for score in train_scores]), np.mean(train_scores), np.mean(train_worse)))
         print("q0 acc %.0f, avgmrr %.6f, worse decisions %.0f" % 
-            (np.sum([1 if score == 1 else 0 for score in train_q0_scores]), np.mean(train_q0_scores), np.sum(train_q0_worse)))
+            (np.mean([1 if score == 1 else 0 for score in train_q0_scores]), np.mean(train_q0_scores), np.mean(train_q0_worse)))
         print("q1 acc %.0f, avgmrr %.6f, worse decisions %.0f" % 
-            (np.sum([1 if score == 1 else 0 for score in train_q1_scores]), np.mean(train_q1_scores), np.sum(train_q1_worse)))
+            (np.mean([1 if score == 1 else 0 for score in train_q1_scores]), np.mean(train_q1_scores), np.mean(train_q1_worse)))
         print("q2 acc %.0f, avgmrr %.6f, worse decisions %.0f" % 
-            (np.sum([1 if score == 1 else 0 for score in train_q2_scores]), np.mean(train_q2_scores), np.sum(train_q2_worse)))
+            (np.mean([1 if score == 1 else 0 for score in train_q2_scores]), np.mean(train_q2_scores), np.mean(train_q2_worse)))
+        print("oracle acc %.0f, avgmrr %.6f, worse decisions %.0f" % 
+            (np.mean([1 if score == 1 else 0 for score in train_oracle_scores]), np.mean(train_oracle_scores), np.mean(train_oracle_worse)))
         print(train_correct)
         print(train_q0_correct)
         print(train_q1_correct)
         print(train_q2_correct)
+        print(train_oracle_correct)
+        print("avg loss", np.mean(agent.loss_history))
 
         ## test
-        test_scores, test_q0_scores, test_q1_scores, test_q2_scores = [],[],[],[]
-        test_worse, test_q0_worse, test_q1_worse,test_q2_worse = [],[],[],[]
-        test_correct, test_q0_correct, test_q1_correct, test_q2_correct = [],[],[],[]
+        test_scores, test_q0_scores, test_q1_scores, test_q2_scores, test_oracle_scores = [],[],[],[],[]
+        test_worse, test_q0_worse, test_q1_worse,test_q2_worse, test_oracle_worse = [],[],[],[],[]
+        test_correct, test_q0_correct, test_q1_correct, test_q2_correct, test_oracle_correct = [],[],[],[],[]
         # test the agent
-        for conv_serial, test_id in enumerate(test_ids):
+        agent.epsilon = 0
+        for test_serial, test_id in enumerate(test_ids):
             obs = user.initialize_state(test_id)
             ignore_questions = []
             q_done = False
             stop = False
             n_round = 0
-            print('-------- test conversation %.0f/%.0f --------' % (conv_serial, len(test_ids)))
+            print('-------- test conversation %.0f/%.0f --------' % (test_serial + 1, len(test_ids)))
             while not q_done:
                 print('-------- round %.0f --------' % (n_round))
                 if obs in memory.keys():
@@ -289,9 +300,10 @@ if __name__ == '__main__':
                         model_file = 'zoo:pretrained_transformers/model_poly/msdialogquestion',  \
                         encode_candidate_vecs = 'true', eval_candidates ='fixed',  \
                         fixed_candidates_path = '/raid/zhenduow/conversationalQA/data/' + exp_name + '/retrieved_question', \
+                        force_fp16_tokens = True,\
                         human_input=obs, fixed_candidate_vecs = 'replace')
                     answer = Interactive.main(model = 'transformer/polyencoder', \
-                        model_file = 'zoo:pretrained_transformers/model_poly/msdialogmodel',  \
+                        model_file = 'zoo:pretrained_transformers/model_poly/msdialoganswer',  \
                         encode_candidate_vecs = 'true', eval_candidates ='fixed',  \
                         fixed_candidates_path = '/raid/zhenduow/conversationalQA/data/' + exp_name + '/retrieved_answer', \
                         human_input=obs,  fixed_candidate_vecs = 'replace')
@@ -340,9 +352,10 @@ if __name__ == '__main__':
                             model_file = 'zoo:pretrained_transformers/model_poly/msdialogquestion',  \
                             encode_candidate_vecs = 'true', eval_candidates ='fixed',  \
                             fixed_candidates_path = '/raid/zhenduow/conversationalQA/data/' + exp_name + '/retrieved_question', \
+                            force_fp16_tokens = True,\
                             human_input=obs_, fixed_candidate_vecs = 'replace')
                         answer_ = Interactive.main(model = 'transformer/polyencoder', \
-                            model_file = 'zoo:pretrained_transformers/model_poly/msdialogmodel',  \
+                            model_file = 'zoo:pretrained_transformers/model_poly/msdialoganswer',  \
                             encode_candidate_vecs = 'true', eval_candidates ='fixed',  \
                             fixed_candidates_path = '/raid/zhenduow/conversationalQA/data/' + exp_name + '/retrieved_answer', \
                             human_input=obs_,  fixed_candidate_vecs = 'replace')
@@ -359,7 +372,7 @@ if __name__ == '__main__':
                     if action == 0 and answer_reward == 1.0:
                         test_correct.append(test_id)
                     test_worse.append(1 if (action == 0 and answer_reward < float(1/use_top_k) and question_reward == cq_reward) \
-                        or (action == 1 and question_reward == cq_penalty) else 0)
+                        or (action == 1 and answer_reward > 0 and question_reward == cq_penalty) else 0)
                 if n_round == 0:
                     test_q0_scores.append(answer_reward)
                     test_q0_worse.append(1 if answer_reward < float(1/use_top_k) and question_reward == cq_reward else 0)
@@ -390,16 +403,23 @@ if __name__ == '__main__':
         assert len(test_scores) == len(test_q0_scores)
         assert len(test_q1_scores) == len(test_q0_scores)
         assert len(test_q1_scores) == len(test_q2_scores)
+        for oi in range(len(test_scores)):
+            test_oracle_scores.append(max(test_q0_scores[oi], test_q1_scores[oi], test_q2_scores[oi]))
+            test_oracle_worse.append(min(test_q0_scores[oi], test_q1_scores[oi], test_q2_scores[oi]))
+        test_oracle_correct = list(set(test_correct + test_q0_correct + test_q2_correct))
 
-        print("Test epoch %.0f, acc %.0f, avgmrr %.6f, worse decisions %.0f" % 
-            (i, np.sum([1 if score == 1 else 0 for score in test_scores]), np.mean(test_scores), np.sum(test_worse)))
+        print("Test epoch %.0f, acc %.6f, avgmrr %.6f, worse decisions %.6f" % 
+            (i, np.mean([1 if score == 1 else 0 for score in test_scores]), np.mean(test_scores), np.mean(test_worse)))
         print("q0 acc %.0f, avgmrr %.6f, worse decisions %.0f" % 
-            (np.sum([1 if score == 1 else 0 for score in test_q0_scores]), np.mean(test_q0_scores), np.sum(test_q0_worse)))
+            (np.mean([1 if score == 1 else 0 for score in test_q0_scores]), np.mean(test_q0_scores), np.mean(test_q0_worse)))
         print("q1 acc %.0f, avgmrr %.6f, worse decisions %.0f" % 
-            (np.sum([1 if score == 1 else 0 for score in test_q1_scores]), np.mean(test_q1_scores), np.sum(test_q1_worse)))
+            (np.mean([1 if score == 1 else 0 for score in test_q1_scores]), np.mean(test_q1_scores), np.mean(test_q1_worse)))
         print("q2 acc %.0f, avgmrr %.6f, worse decisions %.0f" % 
-            (np.sum([1 if score == 1 else 0 for score in test_q2_scores]), np.mean(test_q2_scores), np.sum(test_q2_worse)))
+            (np.mean([1 if score == 1 else 0 for score in test_q2_scores]), np.mean(test_q2_scores), np.mean(test_q2_worse)))
+        print("oracle acc %.0f, avgmrr %.6f, worse decisions %.0f" % 
+            (np.mean([1 if score == 1 else 0 for score in test_oracle_scores]), np.mean(test_oracle_scores), np.mean(test_oracle_worse)))
         print(test_correct)
         print(test_q0_correct)
         print(test_q1_correct)
         print(test_q2_correct)
+        print(test_oracle_correct)

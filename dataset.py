bdae008707896df2843668699955ce9857c542a3
Zhenduo Wang <30311623+zhenduow@users.noreply.github.com>
2020-10-27 11:53:54 -0600

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -8,23 +8,17 @@ class ConversationDataset():
     The conversation database class. The dataset should consist of a dictionary with the conversation identifiers as keys,
     and lists of utterances as values.
     '''
-    def __init__(self, path_to_dataset, dataset_size):
+    def __init__(self, path_to_dataset):
         self.conversations = {}
         self.responses_pool = []
         self.answers_pool = []
         self.max_len = 512
         all_data_list = glob.glob(path_to_dataset + '*')
-        total = 0
         for data_file in all_data_list:
-            if total > dataset_size:
-                break
-            
-            total += 1
-
             f = open(data_file)
             data = f.readlines()
             data = [d.strip() for d in data]
-            data_id = data_file.split('/')[3]
+            data_id = data_file.split('/')[-1]
             self.conversations[data_id] = data
 
             for ut_num in range(len(data)):

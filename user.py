5241b03e865d24ffc814f1e2b8805dfb6620eb01
Zhenduo Wang <30311623+zhenduow@users.noreply.github.com>
2020-12-01 14:56:03 -0700

    Update user.py

--- user.py
+++ user.py
@@ -37,7 +37,7 @@ class User:
         :param question: (str) The question that needs response from the user simulator.
         :return: (str) The user simulator's response;
                 0, if the question is not in the given conversation, i.e., a bad question.
-                -1, if the question is in the given conversation but not followed up with response in the database. In this case, the question maybe an answer.
+                1, if the question is in the given conversation but not followed up with response in the database. In this case, the question maybe an answer.
         '''
         is_off_topic = True
         question_pos = -1
@@ -53,78 +53,20 @@ class User:
                 return self.dataset[conversation_id][question_pos + 1]
             except:
                 #logging.info('The question is the last utterance in the conversation.')
-                return -1
-    
-    def simulate(self, conversation_id, agent):
-        '''
-        Simulate a whole conversation. The user simulator iteratively respond to the agent's input.
-        The user simulator will quit the conversation if:
-        1. The agent asks too many questions before retrieving answer. (more than self.patience)
-        2. The agent asks too many bad questions before retrieving answer. (more than self.tolerance)
-
-        :return: -1, if the conversation_id is not in the database.
-        '''
-        
-        question_count = 0
-        bad_question_count = 0
-        logging.info('Simulating conversation ' + conversation_id)
-        try:
-            self.dataset[conversation_id]
-        except:
-            logging.info('The conversation ' + conversation_id + ' is not in database.')
-            return -1
-
-        query = self.dataset[conversation_id][0] # initialize the query with the first user post.
-
-        while True:
-            # system process the conversation and generate answer or question.
-            is_answer = False # whether the agent returns an answer, set to True if the agent retrieves answer
-
-            agent_utterance = agent.generate_question(query)
-            logging.info('< ' + agent_utterance)
-
-            if not is_answer:
-                question_count += 1
-                if question_count > self.patience:
-                    query = query + ' ' + agent_utterance + 'I have provided enough information. Can you give me the answer immediately?'
-                    continue
-                cur_response = self.respond_to_question(conversation_id, agent_utterance)
-                if cur_response == -1:
-                    logging.info('The conversation ends here in the database. Or the clarifying question being asked is actually the final answer to the query')
-                    break
-                elif cur_response == 0:
-                    bad_question_count += 1
-                    if bad_question_count > self.tolerance:
-                        query = query + ' ' + agent_utterance + 'That is not relevant to my problem.'
-                        break
-                else:
-                    logging.info('> ' + cur_response)
-                    query = query + ' ' + agent_utterance + ' ' + cur_response # update query status
-
-            elif is_answer == '1':
-                logging.info('Answer received. Evaluating.')
-                pass
-
+                return 1
 
     def initialize_state(self, conversation_id):
         '''
         Initialize the user state given the conversation id.
         '''            
         initial_query = self.dataset[conversation_id][0]
-        initial_query = initial_query[:-1] if initial_query[-1] == '\n' else initial_query
+        try:
+            initial_query = initial_query[:-1] if initial_query[-1] == '\n' else initial_query
+        except:
+            return ''
         self.anger = 0
         return initial_query
-        
 
-    def initialize_pretrain_state(self, conversation_id):
-        '''
-        Initialize the user state given the conversation id.
-        '''
-        final_query = self.dataset[conversation_id][0][:-1] if self.dataset[conversation_id][0][-1] == '\n' else self.dataset[conversation_id][0]    
-        for ut in self.dataset[conversation_id][1:-1]:
-            final_query += ' [SEP] '
-            final_query += ut[:-1] if ut[-1] == '\n' else ut
-        return final_query
     
     def update_state(self, conversation_id, context, action, top_n_question, top_n_answer, use_top_k):
         '''
@@ -135,14 +77,13 @@ class User:
             # agent answer the question, evaluate the answer
             n = len(top_n_answer)
             context_ = context + ' [SEP] ' + top_n_answer[0]
-            rel = [1]+[0]*(n-1)
             true_rel = [0] * n
             for i in range(n):
                 try:
-                    true_rel[i] = - self.respond_to_question(conversation_id, top_n_answer[i])
+                    true_rel[i] = self.respond_to_question(conversation_id, top_n_answer[i])
                 except:
                     print("Error in conversation: " + conversation_id)
-            reward = sum([(n*n)/(id+1) for id,n in enumerate(true_rel)])
+            reward = sum([n/(id+1) for id,n in enumerate(true_rel)])
             if reward > 1:
                 reward = 1
             return context_, reward, True, None
